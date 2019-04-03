a15fc993c7b8b3537a62c04574424d30abe5a14c
Armaldio <armaldio@gmail.com>
2019-04-03 10:36:39 -0400

    improved argument parsing fixed JSON export to file

--- bin/ace.js
+++ bin/ace.js
@@ -1,22 +1,58 @@
 #! /usr/bin/env node
 
-var c2PluginAceTable = require('../lib/index');
-var args             = require('yargs').argv;
-var fs               = require('fs');
-var path             = require('path');
+const c2PluginAceTable = require('../lib/index');
+const fs               = require('fs');
+const path             = require('path');
+const util             = require('util');
 
-if (args.d && args.e) {
-    var ace = c2PluginAceTable.export(args.d, args.e);
-    if (args.d === 'json') {
-        console.log(JSON.stringify(ace, null, '\t'));
-    } else if (args.o) {
-        fs.writeFileSync(path.join(args.d, args.o), ace, 'utf8');
+const args = require('yargs')
+  .option('directory', {
+    alias   : 'd',
+    describe: 'The directory where the plugin is located',
+  })
+  .option('export', {
+    alias   : 'e',
+    default : 'json',
+    describe: 'The type of file to export to',
+    choices : [ 'json', 'html', 'markdown' ],
+  })
+  .option('output', {
+    alias   : 'o',
+    describe: 'The ath to the output file',
+  })
+  .option('type', {
+    alias   : 't',
+    describe: 'Whether to only get the type of addon',
+  })
+  .argv;
+
+// if I have a directory
+if (args.directory) {
+  // If I have an export type
+  if (args.export) {
+    const ace = c2PluginAceTable.export(args.directory, args.export);
+    // If I have an output
+    if (args.output) {
+      // If this output is JSON
+      let output;
+      if (args.export === 'json') {
+        output = JSON.stringify(ace, null, '  ');
+      } else {
+        output = ace;
+      }
+      // console.log(util.inspect(ace, true, 10));
+      fs.writeFileSync(path.join(args.directory, args.output), output, 'utf8');
     } else {
+      if (args.export === 'json') {
+        console.log(JSON.stringify(ace, null, '  '));
+      } else {
         console.log(ace);
+      }
     }
-} else if (args.d && args.t) {
-    var type = c2PluginAceTable.getType(args.d);
+  } else if (args.type) {
+    const type = c2PluginAceTable.getType(args.directory);
     console.log(type);
+  }
 } else {
-    console.log("Missing plugin folder or export type");
+  console.log('Missing plugin folder');
 }
